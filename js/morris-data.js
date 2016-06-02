1ac9d6037610457eed181d1254985650081d8350
Pirso, Peep / Kuehne + Nagel / TLL GI-CP <peep.pirso@kuehne-nagel.com>
2016-06-02 17:38:32 +0300

    Added reporting and adjusted the data for visuals

--- js/morris-data.js
+++ js/morris-data.js
@@ -3,59 +3,59 @@ $(function() {
     Morris.Area({
         element: 'morris-area-chart',
         data: [{
-            period: '2010 Q1',
-            iphone: 2666,
-            ipad: null,
-            itouch: 2647
+            period: '2014 Q1',
+            freight: 2666,
+            sea: null,
+            overland: 2647
         }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
+            period: '2014 Q2',
+            freight: 2778,
+            sea: 2294,
+            overland: 2441
         }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
+            period: '2014 Q3',
+            freight: 4912,
+            sea: 1969,
+            overland: 2501
         }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
+            period: '2014 Q4',
+            freight: 3767,
+            sea: 3597,
+            overland: 5689
         }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
+            period: '2015 Q1',
+            freight: 6810,
+            sea: 1914,
+            overland: 2293
         }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
+            period: '2015 Q2',
+            freight: 5670,
+            sea: 4293,
+            overland: 1881
         }, {
-            period: '2011 Q3',
-            iphone: 4820,
-            ipad: 3795,
-            itouch: 1588
+            period: '2015 Q3',
+            freight: 4820,
+            sea: 3795,
+            overland: 1588
         }, {
-            period: '2011 Q4',
-            iphone: 15073,
-            ipad: 5967,
-            itouch: 5175
+            period: '2015 Q4',
+            freight: 15073,
+            sea: 5967,
+            overland: 5175
         }, {
-            period: '2012 Q1',
-            iphone: 10687,
-            ipad: 4460,
-            itouch: 2028
+            period: '2016 Q1',
+            freight: 10687,
+            sea: 4460,
+            overland: 2028
         }, {
-            period: '2012 Q2',
-            iphone: 8432,
-            ipad: 5713,
-            itouch: 1791
+            period: '2016 Q2',
+            freight: 8432,
+            sea: 5713,
+            overland: 1791
         }],
         xkey: 'period',
-        ykeys: ['iphone', 'ipad', 'itouch'],
-        labels: ['iPhone', 'iPad', 'iPod Touch'],
+        ykeys: ['freight', 'sea', 'overland'],
+        labels: ['Freight', 'Sea', 'Overland'],
         pointSize: 2,
         hideHover: 'auto',
         resize: true
@@ -64,13 +64,13 @@ $(function() {
     Morris.Donut({
         element: 'morris-donut-chart',
         data: [{
-            label: "Download Sales",
+            label: "Overland Sales",
             value: 12
         }, {
-            label: "In-Store Sales",
+            label: "Sea Sales",
             value: 30
         }, {
-            label: "Mail-Order Sales",
+            label: "Freight Sales",
             value: 20
         }],
         resize: true
@@ -79,37 +79,37 @@ $(function() {
     Morris.Bar({
         element: 'morris-bar-chart',
         data: [{
-            y: '2006',
+            y: '2010',
             a: 100,
             b: 90
-        }, {
-            y: '2007',
-            a: 75,
-            b: 65
-        }, {
-            y: '2008',
-            a: 50,
-            b: 40
-        }, {
-            y: '2009',
-            a: 75,
-            b: 65
-        }, {
-            y: '2010',
-            a: 50,
-            b: 40
         }, {
             y: '2011',
             a: 75,
             b: 65
         }, {
             y: '2012',
+            a: 50,
+            b: 40
+        }, {
+            y: '2013',
+            a: 75,
+            b: 65
+        }, {
+            y: '2014',
+            a: 50,
+            b: 40
+        }, {
+            y: '2015',
+            a: 75,
+            b: 65
+        }, {
+            y: '2016',
             a: 100,
             b: 90
         }],
         xkey: 'y',
         ykeys: ['a', 'b'],
-        labels: ['Series A', 'Series B'],
+        labels: ['Inbound', 'Outbound'],
         hideHover: 'auto',
         resize: true
     });
